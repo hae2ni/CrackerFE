964e7aa6c1d8b80ba3bb750b007470e82c5661b2
HAE2NI <haein990222@naver.com>
2023-02-11 21:50:52 +0900

    firstcommit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Cracker Project </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
